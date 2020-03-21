c03478e59b2b69f450ae9d638a0a260f5a10ee73
Christophe Kirkoussis <kirkoussis.christophe@hotmail.com>
2020-03-21 01:49:31 +0100

    logging

---
+++ logger.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(level=logging.WARNING, 
+                    format='%(asctime)s | %(levelname)s : %(message)s')
+
+logging.debug("La fonction a bien été exécutée.")
+logging.info("Message d'information général")
+logging.warning("Attention !")
+logging.error("Une erreur est arrivée")
+logging.critical("Erreur critique")
