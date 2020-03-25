6703a79bafcaeaf7f827925161cef8d8192ce41b
Christophe Kirkoussis <kirkoussis.christophe@hotmail.com>
2020-03-26 00:50:51 +0100

    Currency Converter

---
+++ final_project/app/app.py
@@ -0,0 +1,70 @@
+from PySide2 import QtWidgets
+import currency_converter
+
+class App(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.c = currency_converter.CurrencyConverter()
+        self.setWindowTitle("Convertisseur de devises")
+        self.setup_ui()
+        self.set_default_values()
+        self.setup_connections()
+
+    def setup_ui(self):
+        self.layout = QtWidgets.QHBoxLayout(self)
+        self.cbb_deviseFrom = QtWidgets.QComboBox()
+        self.spn_montant = QtWidgets.QSpinBox()
+        self.cbb_devisesTo = QtWidgets.QComboBox()
+        self.spn_montantConverti = QtWidgets.QSpinBox()
+        self.btn_inverser = QtWidgets.QPushButton("Inverser Devises")
+
+
+        self.layout.addWidget(self.cbb_deviseFrom)
+        self.layout.addWidget(self.spn_montant)
+        self.layout.addWidget(self.cbb_devisesTo)
+        self.layout.addWidget(self.spn_montantConverti)
+        self.layout.addWidget(self.btn_inverser)
+
+    def set_default_values(self):
+        self.cbb_deviseFrom.addItems(sorted(list(self.c.currencies)))
+        self.cbb_devisesTo.addItems(sorted(list(self.c.currencies)))
+        self.cbb_deviseFrom.setCurrentText("EUR")
+        self.cbb_devisesTo.setCurrentText("USD")
+
+        self.spn_montant.setRange(1, 1000000)
+        self.spn_montantConverti.setRange(1, 1000000)
+
+        self.spn_montant.setValue(100)
+        self.spn_montantConverti.setValue(100)
+
+    def setup_connections(self):
+        self.cbb_deviseFrom.activated.connect(self.compute)
+        self.cbb_devisesTo.activated.connect(self.compute)
+        self.spn_montant.valueChanged.connect(self.compute)
+        self.btn_inverser.clicked.connect(self.invert_device)
+
+    def compute(self):
+        montant = self.spn_montant.value()
+        devise_from = self.cbb_deviseFrom.currentText()
+        devise_to = self.cbb_devisesTo.currentText()
+
+        try:
+            result = self.c.convert(montant, devise_from, devise_to)
+        except currency_converter.currency_converter.RateNotFoundError:
+            print("La conversion n'a pas fonctionné, veuillez choisir une autre devise.")
+        else:
+            self.spn_montantConverti.setValue(result)
+
+    def invert_device(self):
+        devise_from = self.cbb_deviseFrom.currentText()
+        devise_to = self.cbb_devisesTo.currentText()
+
+        self.cbb_deviseFrom.setCurrentText(devise_to)
+        self.cbb_devisesTo.setCurrentText(devise_from)
+
+        self.compute()
+
+app = QtWidgets.QApplication([])
+win = App()
+win.show()
+app.exec_()
